41c3f190f1ee088fb32e68c62fef9881fbc831fc
unknown <akreesten@gmail.com>
2025-09-06 17:43:40 +0100

    Changement pour la vue en production

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -6,6 +6,7 @@ use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Http\Resources\ProductResource;
 
 class ShopController extends Controller
 {
@@ -29,7 +30,8 @@ class ShopController extends Controller
         $categories = Category::all();
 
         return Inertia::render('Shop/Index', [
-            'products' => $products,
+            // Correction : Utiliser la ressource pour formater les produits
+            'products' => ProductResource::collection($products),
             'categories' => $categories,
         ]);
     }
@@ -37,17 +39,19 @@ class ShopController extends Controller
     public function show(Product $product)
     {
         $similarProducts = Product::where('id', '!=', $product->id)
-                                    ->where('category_id', $product->category_id)
-                                    ->inRandomOrder()
-                                    ->limit(4)
-                                    ->get();
-                                    
+                                ->where('category_id', $product->category_id)
+                                ->inRandomOrder()
+                                ->limit(4)
+                                ->get();
+                                
         // Charge les commentaires liés au produit, triés par le plus récent
         $comments = $product->comments()->latest()->get();
 
         return Inertia::render('Shop/Show', [
-            'product' => $product->load('images'),
-            'similarProducts' => $similarProducts->load('images'),
+            // Correction 1 : Utiliser la ressource pour le produit principal
+            'product' => new ProductResource($product->load('images')),
+            // Correction 2 : Utiliser la ressource pour les produits similaires
+            'similarProducts' => ProductResource::collection($similarProducts->load('images')),
             'comments' => $comments,
         ]);
     }
